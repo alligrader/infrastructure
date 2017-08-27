1b3a503d84f5e14391db3f5faaf03ca28618f51a
Robbie McKinstry <robbie@hashicorp.com>
2017-08-27 17:18:12 -0400

    have the consul servers join a cluster

---
+++ README.md
@@ -0,0 +1,19 @@
+# About
+
+I need to establish two nodes: 
+
+## Node 1
+
+- A vault client
+
+- Noman server
+
+- Go CI
+
+## Node 2
+
+- Nomad agent
+
+- The actual autograder CI
+
+- any of the running jobs
---
+++ digital_ocean.tf
@@ -0,0 +1,107 @@
+# First, add a variable called do_token,
+# which is populated by my environment
+# see .config/fish/config.fish for the value
+variable "do_token" {}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = "${var.do_token}"
+}
+
+# Link my local ssh key
+resource "digitalocean_ssh_key" "default" {
+  name       = "hashi-macbook"
+  public_key = "${file("/Users/hashi/.ssh/id_rsa.pub")}"
+}
+
+resource "digitalocean_floating_ip" "master" {
+  droplet_id = "${digitalocean_droplet.master.id}"
+  region     = "${digitalocean_droplet.master.region}"
+}
+
+resource "digitalocean_floating_ip" "minion" {
+  droplet_id = "${digitalocean_droplet.minion.id}"
+  region     = "${digitalocean_droplet.minion.region}"
+}
+
+# Provision the nodes once
+# their floating IP addresses have been assigned
+resource "null_resource" "autojoin-consul-master" {
+    depends_on = ["digitalocean_floating_ip.master", "digitalocean_floating_ip.minion"]
+
+    triggers {
+        do_node = "${digitalocean_floating_ip.master.ip_address}"
+    }
+
+    provisioner "remote-exec" {
+        connection {
+            agent = true
+            host =  "${digitalocean_floating_ip.master.ip_address}"
+        }
+
+        inline = [
+            "apt-get update",
+            "apt-get install -yq zip",
+            "curl https://releases.hashicorp.com/nomad/0.6.0/nomad_0.6.0_linux_386.zip >> nomad.zip",
+            "curl https://releases.hashicorp.com/consul/0.9.2/consul_0.9.2_linux_386.zip >> consul.zip",
+            "unzip nomad.zip",
+            "unzip consul.zip",
+            "rm nomad.zip",
+            "rm consul.zip",
+            # "consul join ${digitalocean_floating_ip.minion.ip_address}"
+            "docker run -d consul agent -dev -join=${digitalocean_floating_ip.minion.ip_address}"
+        ]
+    }
+}
+
+# Provision the nodes once
+# their floating IP addresses have been assigned
+resource "null_resource" "autojoin-consul-minion" {
+
+    depends_on = ["digitalocean_floating_ip.master", "digitalocean_floating_ip.minion"]
+
+    triggers {
+        do_node = "${digitalocean_floating_ip.minion.ip_address}"
+    }
+
+    provisioner "remote-exec" {
+        connection {
+            agent = true
+            host =  "${digitalocean_floating_ip.master.ip_address}"
+        }
+
+        inline = [
+            "apt-get update",
+            "apt-get install -yq zip",
+            "curl https://releases.hashicorp.com/nomad/0.6.0/nomad_0.6.0_linux_386.zip >> nomad.zip",
+            "curl https://releases.hashicorp.com/consul/0.9.2/consul_0.9.2_linux_386.zip >> consul.zip",
+            "unzip nomad.zip",
+            "unzip consul.zip",
+            "rm nomad.zip",
+            "rm consul.zip",
+            # "consul join ${digitalocean_floating_ip.master.ip_address}"
+            "docker run -d consul agent -dev -join=${digitalocean_floating_ip.master.ip_address}"
+        ]
+    }
+}
+# Create the master node
+resource "digitalocean_droplet" "master" {
+    image              = "docker-16-04"
+    size               = "512mb"
+    region             = "nyc3"
+    name               = "master"
+    ssh_keys           = ["${digitalocean_ssh_key.default.fingerprint}"]
+    ipv6               = true
+    private_networking = true
+}
+
+# Create the master node
+resource "digitalocean_droplet" "minion" {
+    image              = "docker-16-04"
+    size               = "512mb"
+    region             = "nyc3"
+    name               = "master"
+    ssh_keys           = ["${digitalocean_ssh_key.default.fingerprint}"]
+    ipv6               = true
+    private_networking = true
+}
